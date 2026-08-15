db805c5e97cb3c2913e84f4cb2799659db335a5b
jcvignoli <dev@jcvignoli.com>
2026-08-15 13:26:41 +0200

    added declare(strict_types=1); except in Name class

--- src/Imdb/KeywordSearch.php
+++ src/Imdb/KeywordSearch.php
@@ -7,6 +7,7 @@
 # This program is free software; you can redistribute and/or modify it      #
 # under the terms of the GNU General Public License (see doc/LICENSE)       #
 #############################################################################
+declare(strict_types=1);
 
 namespace Imdb;
 
--- src/Imdb/Logger.php
+++ src/Imdb/Logger.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Imdb;
 
 use Psr\Log\LoggerInterface;
